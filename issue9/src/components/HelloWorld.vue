0919031a2b028cc41a3475b3a1885621fcb2fca3
timtnleeProject <litingen1995@gmail.com>
2019-04-03 09:43:16 +0800

    add issue9

---
+++ issue9/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <div>rotation: {{rotation.toFixed(2)}}deg</div>
+    <clipper-range v-model="rotation" :min="0" :max="360"></clipper-range>
+    <clipper-fixed :src="src" :rotate="rotation"></clipper-fixed>
+  </div>
+</template>
+
+<script>
+import { clipperFixed, clipperRange } from "vuejs-clipper";
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    clipperFixed,
+    clipperRange
+  },
+  data: () => {
+    return {
+      src: "bird.jpg",
+      rotation: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  max-width: 500px;
+}
+</style>
---
+++ issue9/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueRx from 'vue-rx'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRx)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
